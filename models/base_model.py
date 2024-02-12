026098cc3de30125886cded8eb2c883674df4b4b
sbixla00 <ayacheayoub25@gmail.com>
2024-02-11 18:59:17 -0800

    dsdq

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
-from engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 from datetime import datetime, timezone
 
 class BaseModel:
